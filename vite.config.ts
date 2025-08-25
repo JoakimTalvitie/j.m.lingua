f0d3dfc676f6286a21eb3fae342bff69d4553797
Reda Lemeden <git@redalemeden.com>
2025-08-25 14:33:38 +0200

    Update README

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import path from "path";
 import {defineConfig, loadEnv} from "vite";
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, ".", "");
   return {
     base: "/j.m.lingua/", // Set base path for GitHub Pages
-    define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
+    define: {},
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
